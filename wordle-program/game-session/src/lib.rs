c87e80f55c876a163051f383183ae37705c4d4d5
TNTBOME <wahaha788@outlook.com>
2024-08-21 21:04:09 +0800

    Update lib.rs

--- wordle-program/game-session/src/lib.rs
+++ wordle-program/game-session/src/lib.rs
@@ -16,11 +16,10 @@ fn get_game_session() -> &'static GameSession {
 extern "C" fn init() {
     let config: GameSessionInit = msg::load().expect("Unable to decode GameSessionInit");
     config.assert_valid();
-    // debug!("{:?}",config);
     let game_session = GameSession::from(config);
     unsafe {
         GAME_SESSION_STATE = Some(game_session);
-        debug!("Initialized game session{:?},",GAME_SESSION_STATE);
+        debug!("Initialized game session: {:?}", GAME_SESSION_STATE);
     }
 }
 
@@ -34,49 +33,79 @@ extern "C" fn handle() {
 
     match action {
         GameSessionAction::StartGame => {
-            debug!("{:?}",user);
-            msg::send(game_session.wordle_program_id, WordleAction::StartGame { user }, 0)
-                .expect("Unable to send StartGame action to Wordle program");
-            let session_info = SessionInfo {
-                session_id: msg::id(),
-                original_msg_id: msg::id(),
-                send_to_wordle_msg_id: MessageId::default(),
-                tries: 0,
-                session_status: SessionStatus::WaitUserInput,
-            };
-            game_session.sessions.insert(user, session_info);
+            let session = game_session.sessions.entry(user).or_insert_with(|| {
+                SessionInfo {
+                    session_id: msg::id(),
+                    original_msg_id: msg::id(),
+                    send_to_wordle_msg_id: MessageId::default(),
+                    tries: 0,
+                    session_status: SessionStatus::Init,
+                }
+            });
 
-            msg::reply(GameSessionEvent::StartSuccess, 0)
-                .expect("Failed to send StartSuccess event");
+            match &session.session_status {
+                SessionStatus::Init => {
+                    debug!("{:?}", user);
+                    msg::send(game_session.wordle_program_id, WordleAction::StartGame { user }, 0)
+                        .expect("Unable to send StartGame action to Wordle program");
+                    session.session_status = SessionStatus::WaitWordleStartReply;
+                    exec::wait();
+                }
+                SessionStatus::ReplyReceived(_) => {
+                    session.session_status = SessionStatus::WaitUserInput;
+                    msg::reply(GameSessionEvent::StartSuccess, 0)
+                        .expect("Failed to send StartSuccess event");
 
-            msg::send_delayed(
-                exec::program_id(),
-                GameSessionAction::CheckGameStatus { user, session_id: msg::id() },
-                0,
-                200,
-            )
-                .expect("Unable to send CheckGameStatus action");
-            exec::wait();
+                    msg::send_delayed(
+                        exec::program_id(),
+                        GameSessionAction::CheckGameStatus { user, session_id: msg::id() },
+                        0,
+                        200,
+                    )
+                        .expect("Unable to send CheckGameStatus action");
+                }
+                _ => {
+                    debug!("Unexpected session status for StartGame: {:?}", session.session_status);
+                }
+            }
         }
 
         GameSessionAction::CheckWord { word } => {
             let session = game_session.sessions.get_mut(&user)
                 .expect("Session not found");
 
-            if session.tries >= 6 {
-                // 处理游戏失败
-                session.session_status = SessionStatus::GameOver(GameStatus::Lose);
-                msg::reply(GameSessionEvent::GameOver(GameStatus::Lose), 0)
-                    .expect("Failed to send GameOver event");
-                return;
+            match &session.session_status {
+                SessionStatus::WaitUserInput => {
+                    let wordle_action = WordleAction::CheckWord { user, word };
+                    session.session_status = SessionStatus::WaitWordleCheckWordReply;
+                    msg::send(game_session.wordle_program_id, wordle_action, 0)
+                        .expect("Unable to send CheckWord action to Wordle program");
+                    exec::wait();
+                }
+                SessionStatus::ReplyReceived(WordleEvent::WordChecked { correct_positions, contained_in_word ,user: _}) => {
+                    let correct_positions=correct_positions.clone();
+                    let contained_in_word=contained_in_word.clone();
+                    session.tries += 1;
+                    if session.tries >= 6 {
+                        session.session_status = SessionStatus::GameOver(GameStatus::Lose);
+                        msg::reply(GameSessionEvent::GameOver(GameStatus::Lose), 0)
+                            .expect("Failed to send GameOver event");
+                    } else if correct_positions.len() == 5 {
+                        session.session_status = SessionStatus::GameOver(GameStatus::Win);
+                        msg::reply(GameSessionEvent::GameOver(GameStatus::Win), 0)
+                            .expect("Failed to send GameOver event");
+                    } else {
+                        session.session_status = SessionStatus::WaitUserInput;
+                        msg::reply(GameSessionEvent::CheckWordResult {
+                            correct_positions,
+                            contained_in_word,
+                        }, 0).expect("Failed to send CheckWordResult event");
+                    }
+                }
+                _ => {
+                    debug!("Unexpected session status for CheckWord: {:?}", session.session_status);
+                }
             }
-
-            let wordle_action = WordleAction::CheckWord { user, word };
-            session.session_status = SessionStatus::WaitWordleCheckWordReply;
-            session.tries += 1;
-            msg::send(game_session.wordle_program_id, wordle_action, 0)
-                .expect("Unable to send CheckWord action to Wordle program");
-            exec::wait();
         }
 
         GameSessionAction::CheckGameStatus { user, session_id } => {
@@ -92,6 +121,8 @@ extern "C" fn handle() {
                     },
                 };
                 msg::reply(event, 0).expect("Failed to send reply");
+            } else {
+                debug!("Session ID mismatch in CheckGameStatus");
             }
         }
     }
@@ -99,17 +130,17 @@ extern "C" fn handle() {
 
 #[no_mangle]
 extern "C" fn handle_reply() {
-    debug!("asasasasasas");
     let wordle_event: WordleEvent = msg::load().expect("Unable to decode WordleEvent");
     let game_session = get_game_session_mut();
-    let user=wordle_event.get_user();
-    debug!("ggg{:?}",game_session);
-    debug!("g7g{:?}",wordle_event);
+    let user = wordle_event.get_user();
+    debug!("Handling WordleEvent: {:?}", wordle_event);
 
     if let Some(session) = game_session.sessions.get_mut(user) {
-            session.session_status = SessionStatus::ReplyReceived(wordle_event);
-            debug!("ggg{:?}",session);
-            exec::wake(session.original_msg_id).expect("Failed to wake");
+        session.session_status = SessionStatus::ReplyReceived(wordle_event);
+        debug!("Updated session status: {:?}", session);
+        exec::wake(session.original_msg_id).expect("Failed to wake");
+    } else {
+        debug!("Session not found for user: {:?}", user);
     }
 }
 
